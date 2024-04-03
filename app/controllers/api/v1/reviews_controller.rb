8877eb1fb32de79bbd8a8c5e781b77db1587117d
UshaSuwal <usaasuwa@gmail.com>
2024-04-03 17:04:48 +0545

    get api additional

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::ReviewsController < ApplicationController
   def index
     movie = Movie.find_by(mid: params[:movie_id])
     if movie
-    reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
-
-
-      render json: { movie_id: movie.id, reviews_count: reviews.count ,reviews: reviews }
+      reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
+      render json: { movie_id: movie.mid, reviews_count: reviews.count ,reviews: reviews }, status: 200
     else
-      render json: { error: 'Movie not found' }
+      # moviecreate = Tmdb::DetailService.execute(id: params[:movie_id])
+      # if moviecreate
+      #   Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'], poster: moviecreate['poster_path'], status: moviecreate['status'], popularity: moviecreate['popularity'], vote_count: moviecreate['vote_count'], vote_average: moviecreate['vote_average'], budget: moviecreate['budget'], duration: moviecreate['runtime'], release_date: moviecreate['release_date'])
+      #   index()
+      # else
+        render json: {error: "Movie not found"}, status: 404
+      end
     end
   end
 
