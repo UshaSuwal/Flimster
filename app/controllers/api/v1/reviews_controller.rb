105138eb12ff1e85b03ab24a84f96bbcce5d30dc
UshaSuwal <usaasuwa@gmail.com>
2024-04-04 13:41:18 +0545

    get api completed

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,18 +1,36 @@
 class Api::V1::ReviewsController < ApplicationController
-  def index
-    movie = Movie.find_by(mid: params[:movie_id])
-    if movie
-      reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
-      render json: { movie_id: movie.mid, reviews_count: reviews.count ,reviews: reviews }, status: 200
-    else
-      # moviecreate = Tmdb::DetailService.execute(id: params[:movie_id])
-      # if moviecreate
-      #   Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'], poster: moviecreate['poster_path'], status: moviecreate['status'], popularity: moviecreate['popularity'], vote_count: moviecreate['vote_count'], vote_average: moviecreate['vote_average'], budget: moviecreate['budget'], duration: moviecreate['runtime'], release_date: moviecreate['release_date'])
-      #   index()
-      # else
-        render json: {error: "Movie not found"}, status: 404
-      end
+    def index
+        movie = Movie.find_by(mid: params[:movie_id])
+        if movie
+        reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
+        render json: { movie_id: movie.mid, review_count: reviews.count, reviews: reviews }
+
+        else
+        tmdb_movie = fetch_tmdb(params[:movie_id])
+
+            if tmdb_movie['id']
+                Movie.create(
+                mid: tmdb_movie['id'],
+                title: tmdb_movie['title'],
+                original_title: tmdb_movie['original_title'],
+                overview: tmdb_movie['overview'],
+                status: tmdb_movie['status'],
+                release_date: tmdb_movie['release_date'],
+                budget: tmdb_movie['budget'],
+                poster: tmdb_movie['poster_path'],
+                popularity: tmdb_movie['popularity']
+                )
+                index()
+            elsif tmdb_movie['status_code']== 6 || tmdb_movie['status_code']== 34
+                render json: { error: 'Movie not found' }, status: 404
+            end
+        end
+
+    end
+
+    private
+
+    def fetch_tmdb(movie_id)
+      response = Tmdb::DetailService.execute(id: movie_id)
     end
   end
-
-end
